0ff3d1dee0d814210969d7e144ed0d2b61255425
Atharva Srivastava <65837319+atharva162@users.noreply.github.com>
2021-02-20 17:40:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
