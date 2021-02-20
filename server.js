a5bc1c3ceb9614c4321dd1fe103be16b8ea1f5f0
Atharva Srivastava <65837319+atharva162@users.noreply.github.com>
2021-02-20 12:23:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 const url = process.env.DB;
