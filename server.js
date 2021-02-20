ff29238e251cd83167f2c374d0a155092112e9d3
Atharva Srivastava <65837319+atharva162@users.noreply.github.com>
2021-02-20 12:57:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +7,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const url = process.env.DB;
 
 mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true,useCreateIndex: true});
